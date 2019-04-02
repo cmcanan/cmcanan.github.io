27c253b3a6f3b7c3be6c88ec80d2bbbd371ea68e
cmcanan <charles_canan@yahoo.com>
2019-04-02 11:40:23 -0500

    Begin

---
+++ map3/webmap3.js
@@ -0,0 +1,5 @@
+let SC_county = L.map('map3').setView([39.075916, -94.587954], 4)
+var OpenMapSurfer_Roads = L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(SC_county)
