b5011c77993f869507a94ea2eede0a25623a6548
cmcanan <charles_canan@yahoo.com>
2019-04-03 20:15:49 -0500

    y

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -3,7 +3,7 @@ var OpenMapSurfer_Roads = L.tileLayer('https://maps.heigit.org/openmapsurfer/til
 	maxZoom: 19,
 	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(SC_county)
-let SCCountyUrl = "https://lsuga.maps.arcgis.com/home/item.html?id=a5a8d9ccbf2f4957aa95a896cac6a1a1.geojson"
+let SCCountyUrl = 'https://lsuga.maps.arcgis.com/home/item.html?id=a5a8d9ccbf2f4957aa95a896cac6a1a1'
 jQuery.getJSON(SCCountyUrl, function (data) {
   L.geoJSON(data).addTo(SC_County)
 })
