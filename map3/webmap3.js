9b17fb37fbe48e63c1da0d4454d104da659cf395
cmcanan <charles_canan@yahoo.com>
2019-04-04 22:14:02 -0500

    Complete

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,22 +1,28 @@
 let SC_county = L.map('map3').setView([33.995698, -81.050963], 7)
 var OpenMapSurfer_Roads = L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  maxZoom: 19,
+  attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(SC_county)
-let SCHospitalPoints = 'https://cmcanan.github.io/map3/SC_Hospitals_Point_data.geojson'
-jQuery.getJSON(SCHospitalPoints, function (data) {
-  L.geoJSON(data).addTo(SC_county)
-})
 let SCCountyLayer = 'https://cmcanan.github.io/map3/DLG_CountyBND.geojson'
 jQuery.getJSON(SCCountyLayer, function (data) {
-	let mapOptions = function (feature) {
-  let stateColor = 'olive'
-  return {
-    color: 'orange',
-    weight: 1,
-    fillOpacity: 0.2
+  let colorStyle = function (feature) {
+		let size = feature.properties.ACRES
+		let countyColor = 'blue'
+		if ( size < 600000 ) { countyColor = 'red'}
+		return {
+      color: countyColor,
+      weight: 2,
+      fillOpacity: 0.2
+    }
   }
-}
-	let styleOptions = { style: mapOptions }
-	L.geoJSON(data, styleOptions).addTo(SC_county)
+  let createCountyPopup = function (feature, layer) {
+	  let name = feature.properties.COUNTYNM
+	  let size = feature.properties.ACRES
+	  layer.bindPopup('Size of ' + name + ' County' + '= ' + size + ' acres' )
+  }
+  let SCCountyGeoJSON = {
+    style: colorStyle,
+    onEachFeature: createCountyPopup
+  }
+  L.geoJSON(data, SCCountyGeoJSON).addTo(SC_county)
 })
